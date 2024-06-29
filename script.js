f105ce14420629a2c3824a6bb01fba59089129a0
Ravi Kumar <heyitsmeravi01@gmail.com>
2024-06-29 17:57:09 +0530

    add features

---
+++ script.js
@@ -0,0 +1,12 @@
+function skills(){
+    let skill=document.getElementById('skills');
+    skill.style.display='block';
+    let education=document.getElementById('education');
+    education.style.display='none'
+}
+function education(){
+    let skills=document.getElementById("skills");
+    skills.style.display='none';
+    let education=document.getElementById("education");
+    education.style.display="block" ;
+}
