0909638c60a2e82c339b4742c38f0ce3024e80b5
Ravi Kumar <heyitsmeravi01@gmail.com>
2024-07-05 23:35:38 +0530

    addedsubmit form with js and some css

--- script.js
+++ script.js
@@ -10,6 +10,29 @@ function education(){
     let education=document.getElementById("education");
     education.style.display="block" ;
 }
-function sendMsg(){
-
+function showdownloadbtn(){
+    let showbtn=document.getElementById('resumedownloadbtn');
+    showbtn.style.display="block";
 }
+
+function sendMsg(){
+    document.querySelector("#msg").innerHTML=" ";
+}
+const form=document.forms['contactData'];
+// const msg=document.getElementById("msg");
+const scriptUrl="https://script.google.com/macros/s/AKfycbz7v0SCljWCYYtxBmhKeUo8KbZpoubKVcDuugFx2emgImxquycfiWDM80OBWFw0L4GJ/exec"
+form.addEventListener("submit",(e)=>{
+    e.preventDefault();
+    document.querySelector(".sendMsg").value="submitting...";
+    let data=new FormData(form);
+    fetch(scriptUrl, {
+        method:"POST", 
+        body: data
+    })
+    .then(res=>res.text())
+    .then(data=> {document.querySelector("#msg").innerHTML="Message sent successfully";
+    document.querySelector(".sendMsg").value="Submit"
+})
+    .catch(error =>console.error('Error!',error.message))
+    
+})
